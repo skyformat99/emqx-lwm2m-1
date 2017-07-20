182f2c6b386f6b6a27631a2d4e4f8ad765ab0752
Lei Chen <alpha_cl@sohu.com>
2017-07-20 15:04:33 +0800

    Fix: backslash at the end of python script.

--- integration_test/insert_lwm2m_plugin.py
+++ integration_test/insert_lwm2m_plugin.py
@@ -19,9 +19,9 @@ def change_makefile():
     if data.find("emq_lwm2m") < 0:
         f = open("emq-relx/relx.config", "wb")
         data = data.replace("{emq_sn, load},", "{emq_sn, load},\n{emq_lwm2m, load},")
-        data = data.replace('{template, 'rel/conf/emq.conf", "etc/emq.conf"},', 
-                '{template, "rel/conf/emq.conf", "etc/emq.conf"},'+
-                '\n    {template, "rel/conf/plugins/emq_coap.conf", "etc/plugins/emq_coap.conf"},'+
+        data = data.replace('{template, "rel/conf/emq.conf", "etc/emq.conf"},', \
+                '{template, "rel/conf/emq.conf", "etc/emq.conf"},'+  \
+                '\n    {template, "rel/conf/plugins/emq_coap.conf", "etc/plugins/emq_coap.conf"},'+  \
                 '\n    {copy, "deps/emq_lwm2m/lwm2m_xml", "etc/"},')
         f.write(data)
         f.close()
@@ -33,8 +33,8 @@ def change_lwm2m_config():
     data = f.read()
     f.close()
     
-    if data.find("lwm2m.port = 5783") > 0:
-        data.replace("lwm2m.port = 5783", "lwm2m.port = 5683")
+    if data.find("5783") > 0:
+        data = data.replace("5783", "5683")
         f = open("emq-relx/deps/emq_lwm2m/etc/emq_lwm2m.conf", "wb")
         f.write(data)
         f.close()
@@ -43,7 +43,7 @@ def change_lwm2m_config():
 
 def main():
     change_makefile()
-    change_lwm2m_config
+    change_lwm2m_config()
     
     
 if __name__ == "__main__":
